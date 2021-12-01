3278a262a326144e44c6f5ded508931681a4c4ea
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-01 11:16:05 +0100

    Format. Lab done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import {useState} from 'react';
-import Contacts from './contacts.json'
-import './App.css';
+import { useState } from "react";
+import Contacts from "./contacts.json";
+import "./App.css";
 
 // Get contacts from position 5 to end position
-const randomContacts = Contacts.slice(5)
+const randomContacts = Contacts.slice(5);
 
 function App() {
-  const [contacts, setContacts] = useState(Contacts.slice(0, 5)) // Set state to first 5 contacts
-  
+  const [contacts, setContacts] = useState(Contacts.slice(0, 5)); // Set state to first 5 contacts
+
   // Add a new random contact from pool to the state contacts
   const addRandomContact = () => {
-    const randomIndex = Math.floor(Math.random() * (randomContacts.length - 5)+ 5)
-    const randomContact = randomContacts.splice(randomIndex, 1)[0]
-    setContacts([...contacts, randomContact])
-  }
-  
+    const randomIndex = Math.floor(
+      Math.random() * (randomContacts.length - 5) + 5
+    );
+    const randomContact = randomContacts.splice(randomIndex, 1)[0];
+    setContacts([...contacts, randomContact]);
+  };
+
   // Sort contacts by highest popularity
   const sortByPopularity = () => {
     const sortedArray = contacts
@@ -37,9 +39,7 @@ function App() {
 
   // Delete selected contact by id
   const deleteContact = (id) => {
-    setContacts(
-      contacts.filter(contact => contact.id !== id)
-    );
+    setContacts(contacts.filter((contact) => contact.id !== id));
   };
 
   return (
@@ -52,26 +52,32 @@ function App() {
       </div>
       <table className="table">
         <tbody>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
-          <th>Actions</th>
-        </tr>
-        {contacts.map(contact => {
-          return (
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+          {contacts.map((contact) => {
+            return (
               <tr key={contact.name}>
-                <td><img src={contact.pictureUrl} alt={contact.name}/></td>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
                 <td>{contact.name}</td>
                 <td>{contact.popularity}</td>
                 {contact.wonOscar ? <td>🏆</td> : <td></td>}
                 {contact.wonEmmy ? <td>🏆</td> : <td></td>}
-                <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
+                <td>
+                  <button onClick={() => deleteContact(contact.id)}>
+                    Delete
+                  </button>
+                </td>
               </tr>
-            )
-        })}
+            );
+          })}
         </tbody>
       </table>
     </div>
